f0173047bf18c76b8fcfc105c84202d40b413710
HerveDjomguem <hdjomguem@gmail.com>
2023-06-19 00:28:20 +0100

    hi

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Admin from './chatbot/Admin';
 import Chatbot from './chatbot/Chatbot';
+
+
 
 const App = () => (
         <div>
             <Router>
             <div className='container'>
-                <Chatbot/>
+      
+            <Routes>
+                    <Route exact path="/admin" element={<Admin/>} />
+                    <Route exact path="" element={<Chatbot/>} />
+                </Routes>
+               
              </div>
             </Router> 
         </div>
---
+++ frontend/src/components/chatbot/Admin.js
@@ -0,0 +1,72 @@
+//Landing 
+import React, { Fragment, useEffect, useState } from 'react';
+import { Table} from 'react-bootstrap';
+import AllMessaages from './AllMessages';  
+import axios from 'axios';
+//Geré ce comportement bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Admin = () => {
+    const [empdata, empdatachange] = useState(null);
+
+ /*   useEffect( async() => {
+        try {
+            const res = await axios.get('/api/comments');
+          //  return res.json();
+            empdatachange(res);
+          } catch (error) {
+            console.log(error.message);
+          }
+        
+    }, [])*/
+
+    useEffect(() => {
+        fetch("http://localhost:8000/comments").then((res) => {
+            return res.json();
+        }).then((resp) => {
+            empdatachange(resp);
+        }).catch((err) => {
+            console.log(err.message);
+        })
+    }, [])
+
+    return (
+        <Fragment>
+            <div style={{margin: "10rem"}}>
+                <div className="card-title">
+                    <h2>DASHBOARD ADMINISTRATEUR</h2>
+                </div>
+              
+                    <Table striped bordered hover size='5m'>
+                        <thead >
+                            <tr>
+                                <th>messageUser</th>
+                                <th>messageBot</th>
+                                <th>date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            {empdata && empdata.length>0
+                              ?
+                                empdata.map(item => (
+                                    <tr key={item.id}>
+                                        <td>{item.messageUser}</td>
+                                        <td>{item.messageBot}</td>
+                                        <td>{item.date}</td>
+                                    </tr>
+                                ))
+                                :
+                                "Aucun message"
+                            }
+
+                        </tbody>
+
+                    </Table>
+            
+            </div>
+        </Fragment>
+    );
+}
+
+export default Admin;
